4ed1ad91f55b78c910af01c816c748e108d1606c
nwallen <nicholas.a.wallen@gmail.com>
2016-05-15 21:43:30 -0700

    UI Tweaks and Currency Formatting

--- Tippit/ViewController.swift
+++ Tippit/ViewController.swift
@@ -15,10 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var tipContainer: UIView!
+    
+    let currencyFormatter = NSNumberFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        hideTip()
+        billField.becomeFirstResponder()
+        
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.currencyCode = NSLocale.currentLocale().displayNameForKey(NSLocaleCurrencySymbol, value: NSLocaleCurrencyCode)
     }
 
     @IBAction func onTap(sender: AnyObject) {
@@ -29,17 +36,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func hideTip() {
+        self.tipContainer.alpha = 1
+        self.tipContainer.frame.origin.y = 400
+        self.billField.frame.origin.y = 180
+    }
+    
+    func showTip() {
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut , animations: {
+            self.billField.frame.origin.y = 110
+            self.tipContainer.alpha = 1
+            self.tipContainer.frame.origin.y = 170
+        }, completion: nil)
+    }
 
     @IBAction func calculateTip(sender: AnyObject) {
         
         let tipPercentages = [0.18, 0.2, 0.25]
         
-        let bill = Double(billField.text!) ?? 0
+        let billText = billField.text!.stringByReplacingOccurrencesOfString(currencyFormatter.currencySymbol, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.groupingSeparator, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.decimalSeparator, withString: "")
+        
+        let bill = Double(billText) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = tip + bill
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipLabel.text = currencyFormatter.stringFromNumber(tip / 100.0)
+        totalLabel.text = currencyFormatter.stringFromNumber(total / 100.0)
+        billField.text = currencyFormatter.stringFromNumber(bill / 100.0)
+        
+        showTip()
         
         
     }
